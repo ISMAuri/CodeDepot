de8111e77e148d54beb3d1674c077e282dcc415a
Ismauri <imcastillocastro@gmail.com>
2023-06-22 19:44:19 -0600

    SkillStack

---
+++ JAVASCRIPT-2/switch.js
@@ -0,0 +1,23 @@
+
+let fruit = 'banaasfsana';
+
+switch(fruit) {
+    case 'apple': 
+        alert('An apple.');
+        break;
+
+    case 'banana': 
+        alert('A banana.');
+        break;
+
+    case 'pear': 
+        alert('A pear.');
+        break;
+
+    default:
+        alert('...');
+        break;
+} 
+
+
+// Just like if, else if, else if, else
